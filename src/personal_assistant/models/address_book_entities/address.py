2ee2a5c9b35ae471e214ea8da57a9cb689dcd405
ver <magictimejkrowling@gmail.com>
2025-11-16 10:59:40 +0100

    update address

--- src/personal_assistant/models/address_book_entities/address.py
+++ src/personal_assistant/models/address_book_entities/address.py
@@ -1,10 +1,7 @@
 class HomeAddress:
     def __init__(self, *args):
-        try:
-            # todo: validate address format
-            self.value = " ".join(args)
-        except ValueError:
-            raise ValueError("Invalid format for address.")
+        """Initialize HomeAddress with a variable number of string arguments."""
+        self.value = " ".join(args)
 
     def __str__(self):
         return self.value
